95f1b80de16bd2946a24be41aef2a2a3a2af30d7
fitriandriastutiitb <23220030@std.stei.itb.ac.id>
2021-01-06 02:07:03 +0700

    commit statisticschatbot reply message 1.0.1

--- public/index.php
+++ public/index.php
@@ -52,6 +52,34 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
     }
 
 // kode aplikasi nanti disini
+    //@pepo
+    $data = json_decode($body, true);
+    if(is_array($data['events'])){
+        foreach ($data['events'] as $event)
+        {
+            if ($event['type'] == 'message')
+            {
+                if($event['message']['type'] == 'text')
+                {
+                    // send same message as reply to user
+                    //$result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+                    //kirim sama text dari chat ditambah keterangan
+                    $result = $bot->replyText($event['replyToken'], 'Chat balasan: '.$event['message']['text']);
+
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+
+
+                    $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+            }
+        }
+    }
 
 });
 $app->run();

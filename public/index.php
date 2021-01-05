b366da0513b1ae91969e314eb82171ccf1d3e71d
fitriandriastutiitb <23220030@std.stei.itb.ac.id>
2021-01-06 06:33:10 +0700

    commit statisticschatbot reply message 1.0.13

--- public/index.php
+++ public/index.php
@@ -115,7 +115,7 @@ Kembali ke menu utama dengan reply "menu"
                         echo $url;
                         print_r($obj);
 //                        echo $obj->data[0]->title;
-                        $result = $bot->replyText($event['replyToken'], 'cari tabel statistik '.$keyword.', hasilnya: '.$obj['data-availability'].', list result: '.print_r($obj));
+                        $result = $bot->replyText($event['replyToken'], 'cari tabel statistik '.$keyword.', hasilnya: '.$obj['data-availability'].', list result: '.print_r($obj['data']));
                     }
 
                     else{

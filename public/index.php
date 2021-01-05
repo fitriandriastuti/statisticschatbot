779a297cdbc87a2ce88871d23ed36ae5baac22dc
fitriandriastutiitb <23220030@std.stei.itb.ac.id>
2021-01-06 05:27:11 +0700

    commit statisticschatbot reply message 1.0.12

--- public/index.php
+++ public/index.php
@@ -106,7 +106,7 @@ Kembali ke menu utama dengan reply "menu"
                     }
 
                     elseif(strtolower(substr($event['message']['text'],0,5))=='tabel'){
-                        $keyword = substr($event['message']['text'], strpos($event['message']['text'], "tabel ") + 1);
+                        $keyword = substr($event['message']['text'], strpos($event['message']['text'], " ") + 1);
                         $key_webapibps = '0e4e501e990fd55e10da084c8f6087d5';
                         $json = file_get_contents('https://webapi.bps.go.id/v1/api/list/model/statictable/domain/0000/keyword/'.$keyword.'/key/'.$key_webapibps.'/');
                         $obj = json_decode($json);
